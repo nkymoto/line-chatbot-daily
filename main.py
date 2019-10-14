fbfbaee2678f6f2d17c495c649a8fbc39289ea96
남경임 <forjava92@gmail.com>
2019-10-14 23:24:13 +0900

    make it better

--- main.py
+++ main.py
@@ -51,6 +51,7 @@ def callback():
     return 'OK'
 
 category = ""
+am_pm = ""
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -58,8 +59,7 @@ def handle_message(event):
     if event.reply_token == "00000000000000000000000000000000":
         return
 
-    req_message = event.message.text
-    if req_message == 'daily':
+    if event.message.text == 'daily':
         button_template = TemplateSendMessage(
             alt_text='Button alt text',
             template=ButtonsTemplate(
@@ -77,83 +77,78 @@ def handle_message(event):
         )
         line_bot_api.reply_message(
             event.reply_token, button_template)
+    elif if(event.message.text.isdigit()):
+        category_time = event.message.text
+        dt_now = datetime.datetime.now()
+        source_to_update = {
+            "date" : dt_now.strftime('%Y-%m-%d'),
+            "category" : int(category),
+            "time" : int(category_time),
+            "am_pm" : am_pm
+        }
 
-        @handler.add(PostbackEvent)
-        def handle_button(event):
-            print(event)
-            global category
-            if event.postback.data in ["1","2","3","4"]:
-                category = event.postback.data
-                confirm_template = TemplateSendMessage(
-                    alt_text='Confirm alt text',
-                    template = ConfirmTemplate(
-                        text='please select am or pm?',
-                        actions=[
-                            PostbackAction(label='AM', data='am'),
-                            PostbackAction(label='PM', data='pm'),
-                        ]
-                    )
-                )
-                line_bot_api.reply_message(
-                    event.reply_token, confirm_template)
+        index_id = dt_now.strftime('%Y%m%d') + category + am_pm
+        # catch API errors
+        try:
+            # call the Update method
+            response = elastic.index(
+                index='daily',
+                id=index_id,
+                body=source_to_update
+            )
 
-                @handler.add(PostbackEvent)
-                def handle_confirm(event):
-                    print(event)
-                    am_pm = event.postback.data
-                    if am_pm in ["am","pm"]:
-                        line_bot_api.reply_message(
-                            event.reply_token, TextSendMessage(text='please input time'))
+            # print the response to screen
+            print (response, '\n\n')
+            if response['result'] == "updated":
+                print ("result:", response['result'])
+                print ("Update was a success for ID:", response['_id'])
+                print ("New data:", source_to_update)
+            else:
+                print ("result:", response['result'])
+                print ("Response failed:", response['_shards']['failed'])
+        except Exception as err:
+            print ('Elasticsearch API error:', err)
 
-                        @handler.add(MessageEvent, message=TextMessage)
-                        def handle_text(event):
-                            print(event)
-                            category_time = event.message.text
-                            if(category_time.isdigit()):
-                                dt_now = datetime.datetime.now()
-                                source_to_update = {
-                                    "date" : dt_now.strftime('%Y-%m-%d'),
-                                    "category" : int(category),
-                                    "time" : int(category_time),
-                                    "am_pm" : am_pm 
-                                }
+        time.sleep(1)
+        result = elastic.search(
+            index='daily',
+            body={'query': {'match': {'date': dt_now.strftime('%Y-%m-%d')}}})
+        hits = result['hits']['total']['value']
+        result_hits = 'ヒット数 : ' + str(hits)
 
-                                index_id = dt_now.strftime('%Y%m%d') + category + am_pm
-                                # catch API errors
-                                try:
-                                    # call the Update method
-                                    response = elastic.index(
-                                        index='daily',
-                                        id=index_id,
-                                        body=source_to_update
-                                    )
-
-                                    # print the response to screen
-                                    print (response, '\n\n')
-                                    if response['result'] == "updated":
-                                        print ("result:", response['result'])
-                                        print ("Update was a success for ID:", response['_id'])
-                                        print ("New data:", source_to_update)
-                                    else:
-                                        print ("result:", response['result'])
-                                        print ("Response failed:", response['_shards']['failed'])
-                                except Exception as err:
-                                    print ('Elasticsearch API error:', err)
-
-                                time.sleep(1)
-                                result = elastic.search(
-                                    index='daily',
-                                    body={'query': {'match': {'date': dt_now.strftime('%Y-%m-%d')}}})
-                                hits = result['hits']['total']['value']
-                                result_hits = 'ヒット数 : ' + str(hits)
-
-                                line_bot_api.reply_message(
-                                    event.reply_token,
-                                    TextSendMessage(text=result_hits))
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=result_hits))
     else:
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=req_message))
+
+@handler.add(PostbackEvent)
+def handle_button(event):
+    print(event)
+    global category
+    global am_pm
+    if event.postback.data in ["1","2","3","4"]:
+        category = event.postback.data
+        confirm_template = TemplateSendMessage(
+            alt_text='Confirm alt text',
+            template = ConfirmTemplate(
+                text='please select am or pm?',
+                actions=[
+                    PostbackAction(label='AM', data='am'),
+                    PostbackAction(label='PM', data='pm'),
+                ]
+            )
+        )
+        line_bot_api.reply_message(
+            event.reply_token, confirm_template)
+
+    elif event.postback.data in ["am","pm"]:
+        am_pm = event.postback.data
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text='please input time'))
+
 if __name__ == "__main__":
 #    app.run()
     port = int(os.getenv("PORT"))
